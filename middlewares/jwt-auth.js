e0eb73e8a09cf5db3da2a7b243d1ea69cf0809f2
Henrdrik Bof <johnatanfl@proton.me>
2023-02-25 10:13:39 +0000

    jwt

--- middlewares/jwt-auth.js
+++ middlewares/jwt-auth.js
@@ -41,8 +41,8 @@ function verifyToken(req, res, next) {
 
   // verify the token
   JWT.verify(authToken, process.env.JWTSECRET, (error, user) => {
-    if (error) return res.status(403).json("SESSION EXPIRED! Please logout and login again.");
-    
+    if (!error) return res.status(403).json("SESSION EXPIRED! Please logout and login again.");
+    console.log(error);
     req.user = user;
     next();
   });
